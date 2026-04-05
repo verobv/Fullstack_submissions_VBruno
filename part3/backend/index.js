bd71940328cad80061d4f4783c8a1576f809d2b0
Veronica Bruno <veronicabrunov@gmail.com>
2026-04-05 21:08:49 +0200

    continue testing render

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -7,6 +7,8 @@ const cors = require('cors')
 
 app.use(cors())
 
+app.use(express.static('dist'))
+
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
