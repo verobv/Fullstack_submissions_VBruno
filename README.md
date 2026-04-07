2d255cfbf88d5ba67ed52b8f00cddc977c5cd08d
Veronica Bruno <veronicabrunov@gmail.com>
2026-04-07 13:05:12 +0200

    fix a bug

---
+++ README.md
@@ -0,0 +1,5 @@
+## Exercise 3. 10. Phonebook Backend
+
+Deployed app:
+https://phonebook-bwgt.onrender.com/
+https://phonebook-bwgt.onrender.com/api/persons
--- part3/backend/index.js
+++ part3/backend/index.js
@@ -57,8 +57,8 @@ app.get('/api/notes/:id', (request, response) => {
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
+  const id = Number(request.params.id)
+  notes = notes.filter(note => Number(note.id) !== id)
 
   response.status(204).end()
 })
